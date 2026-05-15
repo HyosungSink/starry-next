70da696c1f580ccb252f3666ed9d390a15384d29
HyosungSink <1119397793@qq.com>
2026-05-15 18:45:29 +0000

    [posix] support pthread mutex syscalls

--- arceos/api/arceos_posix_api/src/imp/pthread/mutex.rs
+++ arceos/api/arceos_posix_api/src/imp/pthread/mutex.rs
@@ -1,6 +1,8 @@
+use alloc::boxed::Box;
+
 use crate::{ctypes, utils::check_null_mut_ptr};
 
-use axerrno::LinuxResult;
+use axerrno::{LinuxError, LinuxResult};
 use axsync::Mutex;
 
 use core::ffi::c_int;
@@ -10,22 +12,48 @@ static_assertions::const_assert_eq!(
     size_of::<ctypes::pthread_mutex_t>(),
     size_of::<PthreadMutex>()
 );
+static_assertions::const_assert_eq!(size_of::<ctypes::pthread_mutex_t>() % size_of::<usize>(), 0);
 
 #[repr(C)]
-pub struct PthreadMutex(Mutex<()>);
+pub struct PthreadMutex {
+    words: [usize; size_of::<ctypes::pthread_mutex_t>() / size_of::<usize>()],
+}
 
 impl PthreadMutex {
     const fn new() -> Self {
-        Self(Mutex::new(()))
+        Self {
+            words: [0; size_of::<ctypes::pthread_mutex_t>() / size_of::<usize>()],
+        }
+    }
+
+    fn ptr(&self) -> usize {
+        self.words[0]
+    }
+
+    fn ptr_mut(&mut self) -> &mut usize {
+        &mut self.words[0]
+    }
+
+    fn init(&mut self) {
+        if self.ptr() == 0 {
+            *self.ptr_mut() = Box::into_raw(Box::new(Mutex::new(()))) as usize;
+        }
+    }
+
+    fn inner(&self) -> LinuxResult<&Mutex<()>> {
+        if self.ptr() == 0 {
+            return Err(LinuxError::EINVAL);
+        }
+        Ok(unsafe { &*(self.ptr() as *const Mutex<()>) })
     }
 
     fn lock(&self) -> LinuxResult {
-        let _guard = ManuallyDrop::new(self.0.lock());
+        let _guard = ManuallyDrop::new(self.inner()?.lock());
         Ok(())
     }
 
     fn unlock(&self) -> LinuxResult {
-        unsafe { self.0.force_unlock() };
+        unsafe { self.inner()?.force_unlock() };
         Ok(())
     }
 }
@@ -40,6 +68,7 @@ pub fn sys_pthread_mutex_init(
         check_null_mut_ptr(mutex)?;
         unsafe {
             mutex.cast::<PthreadMutex>().write(PthreadMutex::new());
+            (*mutex.cast::<PthreadMutex>()).init();
         }
         Ok(0)
     })
