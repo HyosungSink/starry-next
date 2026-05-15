61159003f55b1c66748f5492146058ddc0f92af5
HyosungSink <1119397793@qq.com>
2026-05-15 18:45:33 +0000

    [runtime] relax panic path for contest shutdown

--- arceos/modules/axruntime/src/lang_items.rs
+++ arceos/modules/axruntime/src/lang_items.rs
@@ -3,5 +3,12 @@ use core::panic::PanicInfo;
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     error!("{}", info);
-    axhal::misc::terminate()
+    #[cfg(target_arch = "riscv64")]
+    {
+        axhal::misc::terminate_on_panic()
+    }
+    #[cfg(not(target_arch = "riscv64"))]
+    {
+        axhal::misc::terminate()
+    }
 }
