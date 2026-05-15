89c4cbb860d315a0f6d8f76bf87b55adcea2d856
HyosungSink <1119397793@qq.com>
2026-05-15 18:45:39 +0000

    [syscall] organize utility syscall modules

--- src/syscall_imp/utils/mod.rs
+++ src/syscall_imp/utils/mod.rs
@@ -1,5 +1,11 @@
+mod misc;
 mod system_info;
+mod sysv_ipc;
 mod time;
+mod timerfd;
 
+pub(crate) use self::misc::*;
 pub(crate) use self::system_info::*;
+pub(crate) use self::sysv_ipc::*;
 pub(crate) use self::time::*;
+pub(crate) use self::timerfd::*;
